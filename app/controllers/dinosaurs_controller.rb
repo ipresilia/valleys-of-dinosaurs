20288126b6680d1e5a1e0aca5d2fc2cf1cf4e145
ipresilia <ilsmarie.presilia@hotmail.com>
2017-05-19 16:50:20 +0200

    Edit, update and delete dinosaurs

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -2,4 +2,57 @@ class DinosaursController < ApplicationController
   def index
     @dinosaurs = Dinosaur.all
   end
+
+  def show
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
+
+
+  def create
+    # dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
+
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+    if @dinosaur.save
+       redirect_to @dinosaur
+    else
+       render 'new'
+    end
+  end
+
+
+  def edit
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+
+  def update
+    @dinosaur = Dinosaur.find(params[:id])
+
+    # dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
+
+    if @dinosaur.update_attributes(dinosaur_params)
+      redirect_to @dinosaur
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @dinosaur = Dinosaur.find(params[:id])
+    @dinosaur.destroy
+    redirect_to dinosaurs_path
+  end
+
+  private
+
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :age, :image_url)
+  end
+
+
 end
