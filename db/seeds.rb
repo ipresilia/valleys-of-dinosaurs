19e21cd20a16317fe1340b22538520b7fede0c37
ipresilia <ilsmarie.presilia@hotmail.com>
2017-05-19 13:39:41 +0200

    Display a list of dinosaurs

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 = Dinosaur.create!( { name: "Tyrannosaurus", age: rand(100), image_url: "http://islanublar.jurassicworld.com/media/dinosaurs/tyrannosaurus-rex/tyrannosaurus-rex-info-graphic.png"})
+dino5 = Dinosaur.create!( { name: "Veliciraptor", age: rand(100), image_url: "http://vignette2.wikia.nocookie.net/jurassicpark/images/1/12/Velociraptor-detail-header.png/revision/latest?cb=20150420213742" } )
